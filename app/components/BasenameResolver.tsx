eb804ce5f18fc43e262fab285bee19e18011dea2
Thedongraphix <chrisoketchcn@gmail.com>
2025-08-18 12:16:36 +0300

    New fixes

--- app/components/BasenameResolver.tsx
+++ app/components/BasenameResolver.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { useEffect, useState } from 'react';
-import { getAddress } from '@coinbase/onchainkit/identity';
 import { getName } from '@coinbase/onchainkit/identity';
 import { base } from 'viem/chains';
 
