72cb168aa425781064f070d1e8b11b939ac39992
Thedongraphix <chrisoketchcn@gmail.com>
2025-08-22 13:23:37 +0300

    New ui tweaks

--- app/components/SimpleOffRampFlow.tsx
+++ app/components/SimpleOffRampFlow.tsx
@@ -547,9 +547,6 @@ export function SimpleOffRampFlow({ setActiveTab }: SimpleOffRampFlowProps) {
                     <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
                   </svg>
                 </div>
-                <div className="text-sm">
-                  <p className="text-gray-300">Funds will arrive within 1-2 minutes</p>
-                </div>
               </div>
             </div>
           </div>
