d803895c6ae763a3de8de7666db272f4c7caabb1
Thedongraphix <chrisoketchcn@gmail.com>
2025-09-11 01:11:08 +0300

    CDP variables set

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,14 +2,16 @@
 
 import { type ReactNode } from "react";
 import { MiniKitProvider } from "@coinbase/onchainkit/minikit";
+import { OnchainKitProvider } from "@coinbase/onchainkit";
 import { base } from "viem/chains";
 import { PostHogProvider } from "@/lib/posthog-provider";
 import { ConsoleLoggerInit } from "@/app/components/ConsoleLoggerInit";
 
 export function Providers(props: { children: ReactNode }) {
   return (
-    <MiniKitProvider
+    <OnchainKitProvider
       apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY!}
+      projectId={process.env.NEXT_PUBLIC_CDP_PROJECT_ID!}
       chain={base}
       config={{
         appearance: {
@@ -25,10 +27,28 @@ export function Providers(props: { children: ReactNode }) {
         },
       }}
     >
-      <ConsoleLoggerInit />
-      <PostHogProvider>
-        {props.children}
-      </PostHogProvider>
-    </MiniKitProvider>
+      <MiniKitProvider
+        apiKey={process.env.NEXT_PUBLIC_ONCHAINKIT_API_KEY!}
+        chain={base}
+        config={{
+          appearance: {
+            mode: 'auto',
+            theme: 'default', 
+            name: process.env.NEXT_PUBLIC_ONCHAINKIT_PROJECT_NAME || 'Minisend',
+            logo: process.env.NEXT_PUBLIC_ICON_URL,
+          },
+          wallet: {
+            display: 'modal',
+            termsUrl: 'https://minisend.xyz/terms',
+            privacyUrl: 'https://minisend.xyz/privacy',
+          },
+        }}
+      >
+        <ConsoleLoggerInit />
+        <PostHogProvider>
+          {props.children}
+        </PostHogProvider>
+      </MiniKitProvider>
+    </OnchainKitProvider>
   );
 }
