ee551f77dfed5adb85b7cc062d2fbf98179414bf
Thedongraphix <chrisoketchcn@gmail.com>
2025-11-14 09:27:05 +0300

    Notice

--- app/page.tsx
+++ app/page.tsx
@@ -154,6 +154,18 @@ export default function App() {
         </div>
       </div>
 
+      {/* Maintenance Notice */}
+      <div className="w-full max-w-md mx-auto px-4 mb-4">
+        <div className="bg-yellow-500/10 border border-yellow-500/30 rounded-lg p-3 flex items-start space-x-3">
+          <svg className="w-5 h-5 text-yellow-500 flex-shrink-0 mt-0.5" fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
+          </svg>
+          <div className="flex-1">
+            <p className="text-sm text-yellow-200 font-medium">We are currently undergoing maintenance and will be back before 12pm EAT </p>
+          </div>
+        </div>
+      </div>
+
       <div className="w-full max-w-md mx-auto px-4 pb-6 overflow-visible">
         {saveFrameButton && (
           <div className="flex justify-end mb-4">
